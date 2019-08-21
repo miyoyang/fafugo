e7fcf35f6f88f09c0e2893334e8f545ce4caa196
lin <997049605@qq.com>
2019-08-21 14:42:54 +0800

    new file:   mvnw.cmd 	new file:   pom.xml new file:   src/main/java/com/lxh/fafugo/FafugoApplication.java 	new file:   src/main/java/com/lxh/fafugo/controller/HelloController.java 	new file:   src/main/resources/application.properties 	new file:   src/main/resources/templates/hello.html 	new file:   src/test/java/com/lxh/fafugo/FafugoApplicationTests.java

---
+++ src/main/java/com/lxh/fafugo/FafugoApplication.java
@@ -0,0 +1,13 @@
+package com.lxh.fafugo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FafugoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FafugoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lxh/fafugo/FafugoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lxh.fafugo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FafugoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
