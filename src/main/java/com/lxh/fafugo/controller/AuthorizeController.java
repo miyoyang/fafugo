046d2a74313815169795258f17a16200a2c73939
lin <997049605@qq.com>
2019-08-22 11:45:49 +0800

    add

--- src/main/java/com/lxh/fafugo/controller/AuthorizeController.java
+++ src/main/java/com/lxh/fafugo/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.lxh.fafugo.dto.AccessTokenDTO;
 import com.lxh.fafugo.dto.GithubUser;
 import com.lxh.fafugo.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,14 +13,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String Client_id;
+    @Value("${github.client.secret}")
+    private String Client_secret;
+    @Value("${github.redirect.uri}")
+    private String Redirect_uri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
                            @RequestParam(name = "state")String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("Iv1.3b837036e57ed164");
-        accessTokenDTO.setClient_secret("994843e2dc7e0f99d5f227aa6ebe34a31981ae4f");
+        accessTokenDTO.setClient_id(Client_id);
+        accessTokenDTO.setClient_secret(Client_secret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setRedirect_uri(Redirect_uri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
