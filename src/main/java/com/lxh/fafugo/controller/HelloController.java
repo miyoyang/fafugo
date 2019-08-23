b096c7187bb667029a7e6bd4ae7eedc1405b16c4
lin <997049605@qq.com>
2019-08-23 09:43:56 +0800

    add check cookie

--- src/main/java/com/lxh/fafugo/controller/HelloController.java
+++ src/main/java/com/lxh/fafugo/controller/HelloController.java
@@ -1,15 +1,42 @@
 package com.lxh.fafugo.controller;
 
+import com.lxh.fafugo.mapper.UserMapper;
+import com.lxh.fafugo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class HelloController {
+    @Autowired
+    private UserMapper userMapper;
     @RequestMapping("/hello")
     public String hello(){
         return "index";
+    }
+    @GetMapping("/")
+    public  String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null) {
+            return "index";
+        } else {
+            for (Cookie cookie : cookies) {
+                if ("token".equals(cookie.getName())) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+            return "index";
+        }
     }
 }
--- src/main/java/com/lxh/fafugo/mapper/UserMapper.java
+++ src/main/java/com/lxh/fafugo/mapper/UserMapper.java
@@ -3,10 +3,15 @@ package com.lxh.fafugo.mapper;
 import com.lxh.fafugo.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into user(NAME,ACCOUNT_ID,TOKEN,GMT_CREATE,GMT_MODIFIED) value(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token=#{token}")
+    User findByToken(@Param("token") String token);
 
 }
